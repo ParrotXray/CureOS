16b8a7d37cab82351808a7973521d74c314312b1
ParrotXray <87219725+ParrotXray@users.noreply.github.com>
2024-05-17 16:35:00 +0800

    Add files via upload

---
+++ 6. virtual memory/includes/libc/stdlib.h
@@ -0,0 +1,5 @@
+char* __uitoa_internal(unsigned int value, char* str, int base, unsigned int* size);
+char* __itoa_internal(int value, char* str, int base, unsigned int* size);
+
+char* itoa(int value, char* str, int base);
+
