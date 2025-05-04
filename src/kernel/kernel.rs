b19fd1790d07edfee48818911b0c9de6fd67cb2d
ParrotXray <a0983805812@gmail.com>
2025-05-05 00:43:29 +0800

    feat: Try Rust

---
+++ src/kernel/kernel.rs
@@ -0,0 +1,20 @@
+// src/kernel/kernel.rs
+use core::arch::asm;
+
+// boot.S 調用的入口點
+#[no_mangle]
+pub extern "C" fn _kernel_init() {
+    // TODO: 加載 GDT
+    // TODO: 加載 IDT  
+    // TODO: 啟用分頁
+}
+
+#[no_mangle]
+pub extern "C" fn _kernel_main(multiboot_info: u32) {
+    
+    unsafe {
+        loop {
+            asm!("hlt");
+        }
+    }
+}
